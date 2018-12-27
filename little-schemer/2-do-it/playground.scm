a75d3d455c98bb9d801525c1b88a8b1ab69a996f
Zach Schuermann <zachary.zvs@gmail.com>
2018-12-27 12:43:20 -0600

    getting used to this whole magit thing

---
+++ little-schemer/2-do-it/playground.scm
@@ -0,0 +1,27 @@
+
+; check if something is in a list of atoms
+(define member?
+  (lambda (a l)
+    (cond
+     ((null? l) #f)
+     (else (or (eq? (car l) a) (member? a (cdr l)))))))
+
+; remove a member from a list of atoms
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((eq? (car lat) a) (cdr lat))
+     (else (cons a (rember (cdr lat)))))))
+
+(rember 'a '(a v d))
+
+; check if something is a list of atoms
+(define lat?
+  (lambda (l)
+    (cond
+     ((null? l) #t)
+     (else (and (atom? (car l)) (lat? (cdr l)))))))
+
+(lat? '(a b c '(d e)))
+(lat? '(a c v dsa vd j))
