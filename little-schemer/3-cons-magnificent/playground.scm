1b264e7e4283ea84736b6fd29713e4e30125c488
Zach Schuermann <zachary.zvs@gmail.com>
2019-01-25 00:19:56 -0600

    Working on chapter 3

--- little-schemer/3-cons-magnificent/playground.scm
+++ little-schemer/3-cons-magnificent/playground.scm
@@ -1,2 +1,56 @@
 ; cons-ing lists
+(load "../prereqs.scm")
+; there was more stuff about rember
+
+;;; Second commandment: use 'cons' to build lists
+
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     (else (cond
+            ((eq? (car lat) a) (cdr lat))
+            (else (cons (car lat) (rember a (cdr lat)))))))))
+
+(define remberall
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     (else (cond
+            ((eq? (car lat) a) (remberall a (cdr lat)))
+            (else (cons (car lat) (remberall a (cdr lat)))))))))
+
+;; I'm an animal
+
+(rember 'a '(b b d a c a e))
+(remberall 'a '(b b d a c a e))
+
+;; Simplification:
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((eq? (car lat) a) (cdr lat))
+     (else (cons (car lat) (rember a (cdr lat)))))))
+
+(rember 'a '(b b d a c a e))
+
+
+; write the function "firsts"
+(define firsts
+  (lambda l
+    (cond
+     ((null? l) '())
+     (else (cons (car (car l) (firsts (cdr l))))))))
+
+(define l '((apple peach poo)
+            (plum pear sherry)
+            (grape rasin pee)
+            (bean carrot eggzima)))
+
+(firsts l)
+
+
+
+
 
---
+++ little-schemer/README.md
@@ -0,0 +1,3 @@
+# The Little Schemer Implementation
+
+Add 10 commandments and 5 laws here. 
