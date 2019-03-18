7f536e12991444ba998548419401a04686f02912
Zach Schuermann <zachary.zvs@gmail.com>
2019-03-17 19:54:30 -0500

    chapter 5 beginning

--- little-schemer/5-full-of-stars/playground.scm
+++ little-schemer/5-full-of-stars/playground.scm
@@ -1,2 +1,17 @@
 ;;; *Oh My Gawd*: It's Full of Stars
 
+(define rember*
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((atom? (car lat))
+      (cond
+       ((eq? (car lat) a) (rember* a (cdr lat)))
+       (else (cons (car lat) (rember* a (cdr lat))))))
+     (else (cons (rember* a (car lat)) (rember* a (cdr lat)))))))
+
+(define l '((coffee) cup ((tea) cup) (and (hick)) cup))
+(rember* 'cup l)
+
+
+      
