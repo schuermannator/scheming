b133b314950b72679547f1f2ea333e18e7d64091
Zach Schuermann <zachary.zvs@gmail.com>
2019-03-18 21:56:46 -0600

    Chapter 5 work

--- little-schemer/5-full-of-stars/playground.scm
+++ little-schemer/5-full-of-stars/playground.scm
@@ -13,5 +13,29 @@
 (define l '((coffee) cup ((tea) cup) (and (hick)) cup))
 (rember* 'cup l)
 
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons old (cons new (insertR* new old (cdr l)))))
+         (else (cons (car l) (insertR* new old (cdr l))))))
+      (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
 
-      
+(define li '((how much (wood)) could ((a (wood) chuck)) (((chuck))) (if (a) ((wood chuck))) could chuck wood))
+  
+(insertR* 'roast 'chuck li)
+
+(define occur*
+  (lambda (a l)
+    (cond 
+      ((null? l) 0)
+      ((atom? (car l))
+       (cond 
+         ((eq? (car l) a) (+ 1 (occur* a (cdr l))))
+         (else (occur* a (cdr l)))))
+      (else (+ (occur* a (car l)) (occur* a (cdr l)))))))
+
+(define ll '((banana) (split ((((banana ice))) (cream (banana)) sherbert)) (banana) (bread) (banana brandy)))
+(occur* 'banana ll)
